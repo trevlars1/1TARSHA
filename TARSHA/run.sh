91f96f432f9eff06815883838cb5542825c3bacd
trevlars <79064068+trevlars@users.noreply.github.com>
2025-04-19 09:30:59 -0400

    more

--- TARSHA/run.sh
+++ TARSHA/run.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-python main.py
+
+#!/usr/bin/with-contenv bashio
+
+# Get the key from HA config
+OPENAI_API_KEY=$(bashio::config 'openai_api_key')
+
+# Inject it into config.ini
+sed -i "s|api_key = .*|api_key = ${OPENAI_API_KEY}|" /app/config.ini
+
+# Start TARS
+python3 app.py
